eb9d74445a11d7545a4dcacd91047f7ee2b9aa85
Asapanna Rakesh <rakeshark22@gmail.com>
2024-05-24 22:22:06 +0530

    long lived token and example send message

---
+++ scripts/generate_longlived_access_token.py
@@ -0,0 +1,34 @@
+# source: https://developers.facebook.com/docs/facebook-login/guides/access-tokens/get-long-lived
+import os
+from pprint import pprint
+
+import httpx
+from dotenv import load_dotenv
+
+assert load_dotenv()
+
+
+APP_ID = os.environ["APP_ID"]
+APP_SECRET = os.environ["APP_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+GRAPH_API_VERSION = "v20.0"
+
+url = "https://graph.facebook.com/{graph_api_version}/oauth/access_token"
+params = {
+    "grant_type": "fb_exchange_token",
+    "client_id": APP_ID,
+    "client_secret": APP_SECRET,
+    "fb_exchange_token": ACCESS_TOKEN,
+}
+
+response = httpx.get(
+    url.format(graph_api_version=GRAPH_API_VERSION), 
+    params=params
+)
+
+# Contains 60 days access token
+pprint(response.json())
+
+# {'access_token': 'xxxxxXVAxdrYI9eZBktzy2ZBnUHjiLBTexxxxxxiAV9OwzqemMURqSSxxxxxxqHq3OyrQ48eF3xIZBq6Vxxxxxx',
+#  'expires_in': 5182954,
+#  'token_type': 'bearer'}
---
+++ scripts/send_whatsapp_message.py
@@ -0,0 +1,44 @@
+# Source: Whatsapp -> API setup
+import os
+from pprint import pprint
+import httpx
+
+from dotenv import load_dotenv
+
+assert load_dotenv()
+
+# Can use either of these tokens
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+LONG_LIVED_ACCESS_TOKEN = os.environ["LONG_LIVED_ACCESS_TOKEN"]
+
+PHONE_NUMBER_ID = os.environ["PHONE_NUMBER_ID"]
+RECIPIENT_WAID = os.environ["RECIPIENT_WAID"]
+GRAPH_API_VERSION = os.environ["VERSION"]
+
+
+url = f"https://graph.facebook.com/{GRAPH_API_VERSION}/{PHONE_NUMBER_ID}/messages"
+headers = {
+    # "Authorization": f"Bearer {ACCESS_TOKEN}",
+    "Authorization": f"Bearer {LONG_LIVED_ACCESS_TOKEN}",
+    "Content-Type": "application/json"
+}
+data = {
+    "messaging_product": "whatsapp",
+    "to": RECIPIENT_WAID,
+    "type": "template",
+    "template": {
+        "name": "hello_world",
+        "language": {
+            "code": "en_US"
+        }
+    }
+}
+
+response = httpx.post(url, headers=headers, json=data)
+
+pprint(response.json())
+
+# {'contacts': [{'input': '91x90xxxx11x', 'wa_id': '91x90xxxx11x'}],
+#  'messages': [{'id': 'wamid.xxxxxxOTAxxxxxxxxExMzQwQkY5NzQ1xxxx==',
+#                'message_status': 'accepted'}],
+#  'messaging_product': 'whatsapp'}
