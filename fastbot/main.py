3c77dae3d6bdd566cc6194543b23e74526c0a6f3
Asapanna Rakesh <rakeshark22@gmail.com>
2024-05-25 18:57:11 +0530

    landing page

--- fastbot/main.py
+++ fastbot/main.py
@@ -1,16 +1,23 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from fastbot.whatsapp.router import router as whatsapp_router
 
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(whatsapp_router)
 
+templates = Jinja2Templates(directory="templates")
 
 @app.head("/")
-@app.get("/")
-def read_root():
-    return {"status": "online"}
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="index.html"
+    )
